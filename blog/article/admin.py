75735269f9ca9d27137c259819fc959bd06d3333
shawn1wugm <shawn1wugm@gmail.com>
2019-06-15 16:36:21 +0800

    db and some alse

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from article.models import Article, Comment
 
 # Register your models here.
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content','pubDateTime']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    '''
+    list_editable = ['content']<在清單就可以更動內容
+    '''
+    
+    class Meta:
+        model = Comment
+
+admin.site.register(Article)
+admin.site.register(Comment, CommentModelAdmin)
---
+++ blog/populate/local.py
@@ -0,0 +1,10 @@
+'''
+本機端測試資料 pile in
+'''
+
+from populate import admin, user, article
+
+
+admin.populate()
+user.populate()
+article.populate()
