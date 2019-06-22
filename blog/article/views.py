3d94b27856929c0c0132099f233ba8557ed5a952
shawn1wugm <shawn1wugm@gmail.com>
2019-06-22 11:43:52 +0800

    update and delete article

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+
+import article
+from article.forms import ArticleForm
 from article.models import  Article, Comment
 
-# Create your views here.
 
+# Create your views here.
 def article(request):
     '''
     Render the article page
@@ -12,3 +16,76 @@ def article(request):
         articles.update({ article:Comment.objects.filter(article = article)})
     context = {'articles' : articles}
     return render(request, 'article/article.html', context)
+
+def articleCreate(request):
+    '''
+    Create a new article instance
+        1. If method is GET, render an empty form
+        2. If method is POST, perform form validation and display error messages if the form is invalid
+        3. Save the form to the model and redirect the user to the article page
+    '''
+    # get
+    template = 'article/articleCnU.html'
+    if request.method == 'GET':
+        return render(request, template, {'articleForm':ArticleForm()})
+
+    # post
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+
+    articleForm.save()
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
+
+def articleRead(request, articleId):
+    '''
+    Read an article
+        1. Get the "article" instance using "articleId"; redirect to the 404 page if not found
+        2. Render the articleRead template with the article instance and its
+           associated comments
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    context = {
+        'article': article,
+        'comments': Comment.objects.filter(article=article)
+    }
+    return render(request, 'article/articleRead.html', context)
+
+def articleUpdate(request, articleId):
+    '''
+    Update the article instance:
+        1. Get the article to update; redirect to 404 if not found
+        2. Render a bound form if the method is GET
+        3. If the form is valid, save it to the model, otherwise render a
+           bound form with error messages
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    template = 'article/articleCnU.html'
+    if request.method == 'GET':
+        articleForm = ArticleForm(instance=article)
+        return render(request, template, {'articleForm':articleForm})
+
+    # POST
+    articleForm = ArticleForm(request.POST, instance=article)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+
+    articleForm.save()
+    messages.success(request, '文章已修改') 
+    return redirect('article:articleRead', articleId=articleId)
+
+
+def articleDelete(request, articleId):
+    '''
+     Delete the article instance:
+        1. Render the article page if the method is GET
+        2. Get the article to delete; redirect to 404 if not found
+    '''
+    if request.method == 'GET':
+        return redirect('article:article')
+    #post
+    article = get_object_or_404(Article, id=articleId)
+    article.delete()
+    messages.success(request, '文章已刪除')
+    return redirect('article:article')
