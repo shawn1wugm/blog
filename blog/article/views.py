edf910f072fdb376900413838351f68bc90e9eb6
shawn1wugm <shawn1wugm@gmail.com>
2019-06-23 13:30:24 +0800

    user leave messages

--- blog/article/views.py
+++ blog/article/views.py
@@ -102,4 +102,85 @@ def articleSearch(request):
                                       Q(content__icontains=searchTerm))
     context = {'articles':articles, 'searchTerm':searchTerm} 
     return render(request, 'article/articleSearch.html', context)
+
+def articleLike(request, articleId):
+    '''
+    Add the user to the 'likes' field:
+        1. Get the article; redirect to 404 if not found
+        2. If the user does not exist in the "likes" field, add him/her
+        3. Finally, call articleRead() function to render the article
     
+    :param request:
+    :param articleId:
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    if request.user not in article.likes.all():
+        article.likes.add(request.user)
+    return articleRead(request, articleId)
+    
+def commentCreate(request, articleId):
+    '''
+    Create a comment for an article:
+        1. Get the "comment" from the HTML form
+        2. Store it to database
+    '''
+    if request.method == 'GET':
+        return articleRead(request, articleId)
+
+    # POST
+    comment = request.POST.get('comment')
+    if comment:
+        comment = comment.strip()
+    if not comment:
+        return redirect('article:articleRead', articleId=articleId)
+
+    article = get_object_or_404(Article, id=articleId)
+    Comment.objects.create(article=article, user=request.user, content=comment)
+    return redirect('article:articleRead', articleId=articleId)
+
+def commentUpdate(request, commentId):
+    '''
+    Update a comment:
+        1. Get the comment to update and its article; redirect to 404 if not found
+        2. If it is a 'GET' request, return
+        3. If the comment's author is not the user, return
+        4. If comment is empty, delete the comment, else update the comment
+    '''
+    commentToUpdate = get_object_or_404(Comment, id=commentId)
+    article = get_object_or_404(Article, id=commentToUpdate.article.id)
+    if request.method == 'GET':
+        return articleRead(request, article.id)
+
+    # POST
+    if commentToUpdate.user != request.user:
+        messages.error(request, '無修改權限')
+        return redirect('article:articleRead', articleId=article.id)
+
+    comment = request.POST.get('comment', '').strip()
+    if not comment:
+        commentToUpdate.delete()
+    else:
+        commentToUpdate.content = comment
+        commentToUpdate.save()
+    return redirect('article:articleRead', articleId=article.id)
+
+def commentDelete(request, commentId):
+    '''
+    Delete a comment:
+        1. Get the comment to update and its article; redirect to 404 if not found
+        2. If it is a 'GET' request, return
+        3. If the comment's author is not the user, return
+        4. Delete the comment
+    '''
+    comment = get_object_or_404(Comment, id=commentId)
+    article = get_object_or_404(Article, id=comment.article.id)
+    if request.method == 'GET':
+        return articleRead(request, article.id)
+
+    # POST
+    if comment.user != request.user:
+        messages.error(request, '無刪除權限')
+        return redirect('article:articleRead', articleId=article.id)
+
+    comment.delete()
+    return redirect('article:articleRead', articleId=article.id)
--- blog/populate/admin.py
+++ blog/populate/admin.py
@@ -5,7 +5,7 @@ from account.models import User
 def populate(): 
     print('Creating admin account ... ', end='')
     User.objects.all().delete()
-    User.objects.create_superuser(username='admin', password='0000', email=None)
+    User.objects.create_superuser(username='admin', password='0000', email=None, fullName='管理者')
     print('done')
 
 
